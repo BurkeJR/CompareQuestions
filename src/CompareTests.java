14ec0d7f4a1eb62ebfd1cd917276738d4707eea7
John Burke <burkejr19@gcc.edu>
2021-03-07 14:52:42 -0500

    began description of CompareTests. added main method in main

---
+++ src/CompareTests.java
@@ -0,0 +1,17 @@
+
+public class CompareTests {
+
+	/*
+	 * Takes an array of Tests and compares the questions in each of them.
+	 * Compares every question a test with all of the questions in every other test.
+	 * Does not compare questions within one Test.
+	 * Records questions that are similar at a given percentage.
+	 */
+	
+	private Test[] tests;
+	private double percentage;
+	
+	public CompareTests() {
+		
+	}
+}
